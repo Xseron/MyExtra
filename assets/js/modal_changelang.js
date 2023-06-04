3679c989273bdaeb31f9e7adcee88f03aa23c33d
virg1n <113487825+virg1n@users.noreply.github.com>
2023-06-04 13:29:34 +0600

    languages to cookies

--- assets/js/modal_changelang.js
+++ assets/js/modal_changelang.js
@@ -1,11 +1,26 @@
 const languages = ["Russian", "English", "Kazakh"]
 let current = 0
 
-
-if( !localStorage.getItem("lang") ){
+if( !getCookie("language") ){
     $(`.popup`).css({"display":"block"})
 }
 
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
 
 // function changeLanguage(cur, delta){
 //     if (cur+delta >= languages.length){
@@ -30,7 +45,7 @@ function openPopupWithLanguage(){
 }
 
 function pickLanguage(lang){
-    localStorage.setItem("lang", lang)
+    document.cookie = encodeURIComponent("language") + '=' + encodeURIComponent(lang);
     $(`.popup`).css({"display":"none"})
 }
 
